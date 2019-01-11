4a08760d391df9740661e3a2c44755bad5ee70af
Leidinice Silva <leidinicesilva@gmail.com>
2019-01-11 14:50:47 -0300

    modified and add new codes

--- plot_time_series_cmip5.py
+++ plot_time_series_cmip5.py
@@ -3,7 +3,7 @@
 __author__      = "Leidinice Silva"
 __email__       = "leidinicesilvae@gmail.com"
 __date__        = "01/08/2019"
-__description__ = "This script plot boxplot and time series from CMIP5"
+__description__ = "This script plot boxplot and time series from CMIP5 models"
 
 
 import netCDF4
@@ -12,15 +12,14 @@ import numpy as np
 from pylab import *
 from netCDF4 import Dataset
 
-
-model = 'bcc-csm1-1'
 var   = 'pr'
-	
-mod_path = '/home/nice/Downloads/{0}'.format(model.upper())
-arq1     = '{0}/{1}_Amon_{2}_historical_r1i1p1_185001-201212.nc'.format(mod_path, var, model)	
+model = 'BCC-CSM1-1'
+exp   = 'historical_r1i1p1'
+data  = '198001-200512'
+
+mod_path = '/home/nice/Downloads'
+arq1     = '{0}/{1}_amz_neb_Amon_{2}_{3}_mon_{4}.nc'.format(mod_path, var, model, exp, data)	
 data1    = netCDF4.Dataset(arq1)
-print data1
-exit()
 
 var1     = data1.variables['pr'][:]
 lat      = data1.variables['lat'][:]
@@ -29,7 +28,7 @@ mod      = var1[:][:,:,:]
 mod_ini1 = np.nanmean(mod, axis=1)
 mod_end1 = np.nanmean(mod_ini1, axis=1)
 
-print mod_end1
+print len(mod_end1)
 print
 
 exit()
