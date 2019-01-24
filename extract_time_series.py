4513c4abc75f447356613a3c626336a4253cd564
Leidinice Silva <leidinicesilva@gmail.com>
2019-01-24 11:00:56 -0300

    new modified

--- extract_time_series.py
+++ extract_time_series.py
@@ -8,31 +8,40 @@ __description__ = "This script to extract time series from CMIP5 models"
 
 import netCDF4
 import numpy as np
+import matplotlib.pyplot as plt
+# mpl.use('Agg')
 
-from pylab import *
-from netCDF4 import Dataset
 
 var   = 'pr'
-model = 'BCC-CSM1-1'
+model = 'BCC-CSM1.1'
 exp   = 'historical_r1i1p1'
-data  = '198001-200512'
+data  = '185001-201212'
 
-mod_path = '/vol3/disco1/nice/cmip_data/cmip5_hist/BCC-CSM1.1'
-arq1     = '{0}/{1}_amz_neb_Amon_{2}_{3}_mon_{4}.nc'.format(mod_path, var, model, exp, data)	
+mod_path = '/home/nice/Documentos/cmip/cmip5_hist/BCC-CSM1.1'
+arq1     = '{0}/{1}_Amon_{2}_{3}_{4}.nc'.format(mod_path, var, model, exp, data)
 data1    = netCDF4.Dataset(arq1)
 
-var1     = data1.variables['pr'][:]
+var1     = data1.variables['pr'][:] * 86400
 lat      = data1.variables['lat'][:]
 lon      = data1.variables['lon'][:]
 mod      = var1[:][:,:,:]
 mod_ini1 = np.nanmean(mod, axis=1)
 mod_end1 = np.nanmean(mod_ini1, axis=1)
 
-print len(mod_end1)
-print
+#~ print mod_end1
+#~ print
+#~ exit()
+
+# plot pluma graphs of all models
+fig  = plt.figure(figsize=(12, 10))
+time = np.arange(1, 1956 + 1)
+
+#~ a = plt.plot(time, mod_end1)
+#~ plt.show()
+#~ exit()
+
+mod_end1.write(mod_end1)
+mod_end1.close()
 exit()
-
-print(mod_end1, file=open('test.txt', 'w'))
-
 
 
