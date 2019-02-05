42fc9c50fd9deda99acf98b19e2113a90c72f770
Leidinice Silva <leidinicesilva@gmail.com>
2019-02-05 01:37:32 -0300

    update file

--- plot_time_series_cmip5.py
+++ plot_time_series_cmip5.py
@@ -11,6 +11,7 @@ import numpy as np
 
 from pylab import *
 from netCDF4 import Dataset
+from scipy.stats import norm
 from matplotlib.font_manager import FontProperties
 
 
@@ -59,11 +60,22 @@ def import_obs_clim(database):
 	obs_data = np.nanmean(np.nanmean(value, axis=1), axis=1)
 
 	obs_clim = []
+	obs_clim_p5 = []
+	obs_clim_p95 = []
+
 	for mon in range(1, 12 + 1):
+		
 		obs = np.nanmean(obs_data[mon::12], axis=0)
 		obs_clim.append(obs)
 	
-	return obs_clim
+		obs_p5  = norm.ppf(0.05, loc=np.nanmean(obs_data[mon::12], axis=0), scale=np.nanstd(obs_data[mon::12], axis=0))
+		obs_clim_p5.append(obs_p5)
+		
+		obs_p95  = norm.ppf(0.95, loc=np.nanmean(obs_data[mon::12], axis=0), scale=np.nanstd(obs_data[mon::12], axis=0))
+		obs_clim_p95.append(obs_p95)
+	
+	return obs_clim, obs_clim_p5, obs_clim_p95
+	
 	
 # Import model end obs database climatology
 model  = u'BCC-CSM1.1'
@@ -108,65 +120,65 @@ mdl13_clim = import_cmip5_clim(model)
 model  = u'HadGEM2-CC'
 mdl14_clim = import_cmip5_clim(model)
 
-model  = u'HadGEM2-ES'
+model  = u'INMCM4'
 mdl15_clim = import_cmip5_clim(model)
 
-model  = u'INMCM4'
+model  = u'IPSL-CM5A-LR'
 mdl16_clim = import_cmip5_clim(model)
 
-model  = u'IPSL-CM5A-LR'
+model  = u'IPSL-CM5A-MR'
 mdl17_clim = import_cmip5_clim(model)
 
-model  = u'IPSL-CM5A-MR'
+model  = u'IPSL-CM5B-LR'
 mdl18_clim = import_cmip5_clim(model)
 
-model  = u'IPSL-CM5B-LR'
+model  = u'LASG-FGOALS-G2'
 mdl19_clim = import_cmip5_clim(model)
 
-model  = u'LASG-FGOALS-G2'
+model  = u'LASG-FGOALS-S2'
 mdl20_clim = import_cmip5_clim(model)
 
-model  = u'LASG-FGOALS-S2'
+model  = u'MIROC5'
 mdl21_clim = import_cmip5_clim(model)
 
-model  = u'MIROC5'
+model  = u'MIROC-ESM-CHEM'
 mdl22_clim = import_cmip5_clim(model)
 
-model  = u'MIROC-ESM-CHEM'
+model  = u'MIROC-ESM'
 mdl23_clim = import_cmip5_clim(model)
 
-model  = u'MIROC-ESM'
+model  = u'MPI-ESM-LR'
 mdl24_clim = import_cmip5_clim(model)
 
-model  = u'MPI-ESM-LR'
+model  = u'MPI-ESM-MR'
 mdl25_clim = import_cmip5_clim(model)
 
-model  = u'MPI-ESM-MR'
+model  = u'MRI-CGCM3'
 mdl26_clim = import_cmip5_clim(model)
 
-model  = u'MRI-CGCM3'
+model  = u'NCAR-CCSM4'
 mdl27_clim = import_cmip5_clim(model)
 
-model  = u'NCAR-CCSM4'
+model  = u'NCAR-CESM1-BGC'
 mdl28_clim = import_cmip5_clim(model)
 
-model  = u'NCAR-CESM1-BGC'
+model  = u'NCAR-CESM1-CAM5'
 mdl29_clim = import_cmip5_clim(model)
 
-model  = u'NCAR-CESM1-CAM5'
+model  = u'NorESM1-ME'
 mdl30_clim = import_cmip5_clim(model)
 
-model  = u'NorESM1-ME'
+model  = u'NorESM1-M'
 mdl31_clim = import_cmip5_clim(model)
 
-model  = u'NorESM1-M'
+model  = u'HadGEM2-ES'
 mdl32_clim = import_cmip5_clim(model)
 
 model  = u'ensmean_cmip5'
 mdl33_clim = import_cmip5_clim(model)
 
 database  = u'cru_ts4.02'
-obs1_clim = import_obs_clim(database)
+obs1_clim, obs1_clim_p5 , obs1_clim_p95  = import_obs_clim(database)
 
 # Plot model end obs data climatology
 fig   = plt.figure(figsize=(26, 18))
@@ -180,44 +192,49 @@ time, mdl16_clim, time, mdl17_clim, time, mdl18_clim, time, mdl19_clim,
 time, mdl20_clim, time, mdl21_clim, time, mdl22_clim, time, mdl23_clim, 
 time, mdl24_clim, time, mdl25_clim, time, mdl26_clim, time, mdl27_clim, 
 time, mdl28_clim, time, mdl29_clim, time, mdl30_clim, time, mdl31_clim, 
-time, mdl32_clim, time, mdl33_clim, time, obs1_clim)
+time, mdl32_clim, time, mdl33_clim, time, obs1_clim, time, obs1_clim_p5, 
+time, obs1_clim_p95)
 
-l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13, l14, l15, l16, l17, l18, l19, l20, l21, l22, l23, l24, l25, l26, l27, l28, l29, l30, l31, l32, l33, l34 = plt_l
+l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13, l14, l15, l16, l17, l18, l19, l20, l21, l22, l23, l24, l25, l26, l27, l28, l29, l30, l31, l32, l33, l34, l35, l36 = plt_l
 
-plt.setp(l1,  linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l2,  linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l3,  linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l4,  linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l5,  linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l6,  linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l7,  linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l8,  linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l9,  linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l10, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l11, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l12, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l13, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l14, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l15, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l16, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l17, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l18, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l19, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l20, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l21, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l22, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l23, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l24, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l25, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l26, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l27, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l28, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l29, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l30, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l31, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l32, linewidth=6, markeredgewidth=1, color='silver')
-plt.setp(l33, linewidth=8, markeredgewidth=1, color='gray')
-plt.setp(l34, linewidth=8, markeredgewidth=1, color='black')
+plt.setp(l1,  linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l2,  linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l3,  linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l4,  linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l5,  linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l6,  linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l7,  linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l8,  linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l9,  linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l10, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l11, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l12, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l13, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l14, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l15, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l16, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l17, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l18, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l19, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l20, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l21, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l22, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l23, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l24, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l25, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l26, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l27, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l28, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l29, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l30, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l31, linewidth=3, markeredgewidth=1, color='gainsboro')
+plt.setp(l32, linewidth=8, markeredgewidth=1, color='gray')
+plt.setp(l33, linewidth=8, markeredgewidth=1, color='black')
+plt.setp(l34, linewidth=8, markeredgewidth=1, linestyle='--', color='black')
+plt.setp(l35, linewidth=8, markeredgewidth=1, linestyle='--', color='lightgray')
+plt.setp(l36, linewidth=8, markeredgewidth=1, linestyle='--', color='dimgray')
+
+plt.fill_between(time, obs1_clim_p5, obs1_clim_p95, facecolor='slategray', alpha=0.2, interpolate=True)
 
 fig.suptitle(u'Climatologia de precipitação (1975-2005)', fontsize=30, fontweight='bold', y=0.92)
 
@@ -228,19 +245,18 @@ plt.xticks(time, xaxis, fontsize=30)
 plt.yticks(np.arange(0, 14, 2), fontsize=30)
 plt.tick_params(axis='both', which='major', labelsize=30, length=8, width=2, pad=4, labelcolor='black')
 
-legend = (u'cmip5'.upper(), u'ensmean_cmip5'.upper(), u'cru'.upper())
+legend = (u'CMIP5', u'HadGEM-ES', u'ENSMEAN_CMIP5', u'CRU', u'CRU_p5%', u'CRU_p95%')
 font = FontProperties(size=25)		    
-plt.legend(plt_l[31:], legend, loc='upper center', bbox_to_anchor=(0.5, -0.07), shadow=True, ncol=3, prop=font)
-
+plt.legend(plt_l[30:], legend, loc='upper center', bbox_to_anchor=(0.5, -0.07), shadow=True, ncol=6, prop=font)
 plt.grid(True, which='major', linestyle='--', linewidth='1.4', zorder=0.6)
 			    
 path_out = '/home/nice/Documentos/cmip5_hist'
-name_out = 'climatoligia_cmip5_obs.png'
+name_out = 'clim_cmip5_obs.png'
 
 if not os.path.exists(path_out):
 	create_path(path_out)
 	
-plt.savefig(os.path.join(path_out, name_out), dpi=25, bbox_inches='tight')
+plt.savefig(os.path.join(path_out, name_out), dpi=200, bbox_inches='tight')
 plt.show()
 exit()
 
