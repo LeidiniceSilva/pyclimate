f20cc16445a4a08f7ae3658854c44505394e22c4
Leidinice <leidinicesilva@gmail.com>
2019-08-29 12:13:13 -0300

    add new code to plot maps matopiba

--- plot_maps_rclimdex_matopiba.py
+++ plot_maps_rclimdex_matopiba.py
@@ -10,7 +10,6 @@ import conda
 import netCDF4
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import shapefile as shp
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
@@ -20,6 +19,8 @@ conda_dir = conda_file_dir.split('lib')[0]
 proj_lib = os.path.join(os.path.join(conda_dir, 'share'), 'proj')
 os.environ["PROJ_LIB"] = proj_lib
 
+from matplotlib.path import Path
+from matplotlib.patches import PathPatch
 from mpl_toolkits.basemap import Basemap
 
 
@@ -47,37 +48,7 @@ def read_shapefile(sf):
     
     return df
         
-        
-def plot_map_fill_multiples_ids(comuna, sf, x_lim=None, y_lim=None, color='purple'):
-
-    fig, ax = plt.subplots(figsize=(6,8))
-    plt.title(u'Shapefile MATOPIBA')
-    plt.xlabel(u'Longitude')
-    plt.ylabel(u'Latitude')
-        
-    for shape in sf.shapeRecords():
-        x = [i[0] for i in shape.shape.points[:]]
-        y = [i[1] for i in shape.shape.points[:]]
-        ax.plot(x, y, 'k')
-            
-    for id in comuna:
-        shape_ex = sf.shape(id)
-        x_lon = np.zeros((len(shape_ex.points),1))
-        y_lat = np.zeros((len(shape_ex.points),1))
-        for ip in range(len(shape_ex.points)):
-            x_lon[ip] = shape_ex.points[ip][0]
-            y_lat[ip] = shape_ex.points[ip][1]
-        ax.fill(x_lon,y_lat, color)
-             
-        x0 = np.mean(x_lon)
-        y0 = np.mean(y_lat)
-        plt.text(x0, y0, id, fontsize=10)
-    
-    if (x_lim != None) & (y_lim != None):     
-        plt.xlim(x_lim)
-        plt.ylim(y_lim)
-
-
+       
 def basemap(lat, lon):
 	
 	aux_lon1 = []
@@ -92,18 +63,15 @@ def basemap(lat, lon):
 	new_lat = lat[::-1]
 	new_lon = lon[::-1]
 	
-	map = Basemap(projection='cyl', llcrnrlon=-85., llcrnrlat=-20., urcrnrlon=-15.,urcrnrlat=10., resolution='c')
-	map.drawmeridians(np.arange(-85.,-5.,10.), size=6, labels=[0,0,0,1], linewidth=0.4)
-	map.drawparallels(np.arange(-20.,15.,5.), size=6, labels=[1,0,0,0], linewidth=0.4)
-	map.drawcoastlines(linewidth=1, color='k')
-	map.drawcountries(linewidth=1, color='k')
+	map = Basemap(projection='cyl', llcrnrlon=-52., llcrnrlat=-17., urcrnrlon=-40.,urcrnrlat=-1., resolution='c')
+	map.drawmeridians(np.arange(-52.,-38.,2.), size=6, labels=[0,0,0,1], linewidth=0.4)
+	map.drawparallels(np.arange(-17.,2.,2.), size=6, labels=[1,0,0,0], linewidth=0.4)
 	
 	xin = np.linspace(map.xmin,map.xmax,20) 
 	yin = np.linspace(map.ymin,map.ymax,20) 
-	lons = np.arange(-85.,-5.,0.25) 
-	lats = np.arange(-20.,15.,-0.25) 
+	lons = np.arange(-52.,-38.,0.25) 
+	lats = np.arange(-17.,2.,0.25) 
 	lons, lats = np.meshgrid(new_lon, new_lat)
-
 	xx, yy = map(lons,lats)
 	
 	return map, xx, yy         
@@ -111,10 +79,11 @@ def basemap(lat, lon):
 
 def plot_maps_rclimdex(idx):
 		
-	fig, ax = plt.subplots()
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
 	levs = [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 30, 32]
 
-	plt.title(u'A) Txn (°C)', loc='left', fontsize=8, fontweight='bold')
+	plt.title(u'A) Txn (°C) - MATOPIBA', loc='left', fontsize=8, fontweight='bold')
 	plt.xlabel(u'Longitude', fontsize=8, labelpad=20, fontweight='bold')
 	plt.ylabel(u'Latitude', fontsize=8, labelpad=20, fontweight='bold')
 	map, xx, yy = basemap(lat, lon)
@@ -122,26 +91,54 @@ def plot_maps_rclimdex(idx):
 	cbar = map.colorbar(ticks=levs, drawedges=True, ax=ax)
 	cbar.ax.tick_params(labelsize=6)
 	
+	return ax, plt_map
+	
 		                                  
 # Open and read shapefile from matopiba 
 shp_path = '/home/nice/Documentos/github_projects/shp/shp_matopiba/shp_matopiba.shp'
 sf = shp.Reader(shp_path)
 df = read_shapefile(sf)
-# Plot shapefile from matopiba 
-plot_map_fill_multiples_ids([0, 1, 2, 3], sf, color='purple')
-
+ 
 # Import rclimdex database 
 lat, lon, idx = import_rclimdex('txnETCCDI')
 # Plot rclimdex database
-plot_maps_rclimdex(idx)
+ax, plt_map = plot_maps_rclimdex(idx)
+
+print(sf)
+print 
+print(df)
+
+for shape_rec in sf.shapeRecords():
+	x = [i[0] for i in shape_rec.shape.points[:]]
+	y = [i[1] for i in shape_rec.shape.points[:]]
+	ax.plot(x, y, 'k')
+					
+	vertices = []
+	codes = []
+	pts = shape_rec.shape.points
+	prt = list(shape_rec.shape.parts) + [len(pts)]
+
+	for i in range(len(prt) - 1):
+		for j in range(prt[i], prt[i+1]):
+			vertices.append((pts[j][0], pts[j][1]))
+		codes += [Path.MOVETO]
+		codes += [Path.LINETO] * (prt[i+1] - prt[i] -2)
+		codes += [Path.CLOSEPOLY]
+	clip = Path(vertices, codes)
+	clip = PathPatch(clip, transform=ax.transData)
+		
+for contour in plt_map.collections:
+        contour.set_clip_path(clip)
 
 # Path out to save figure
 path_out = '/home/nice/Documentos/ufrn/PhD_project/results/rclimdex'
-name_out = 'pyplt_maps_txnETCCDI_rclimdex_hadgem_1859-2005.png'
+name_out = 'pyplt_maps_txnETCCDI_rclimdex_hadgem_matopiba_1859-2005.png'
 if not os.path.exists(path_out):
 	create_path(path_out)
 plt.savefig(os.path.join(path_out, name_out), dpi=400, bbox_inches='tight')
 
+plt.show()
+exit()
 
 
 
